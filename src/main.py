e32d9bd19167ab1ddb874a4cb4e9c879df588f73
Balogh David <balogh.david1015@gmail.com>
2018-08-28 10:21:02 +0200

    get product by user_id done, need try except:

--- src/main.py
+++ src/main.py
@@ -28,15 +28,6 @@ def get_all_product():
 
     return query
 
-@app.route("/product")
-def list():
-    data = get_all_product()
-    return build_json(data)
-
-@app.route("/product/<id>")
-def list_one(id):
-    data = get_one_product(id)
-    return build_json(data)
 
 def build_json(data):
     dikt = {}
@@ -63,6 +54,27 @@ def build_json(data):
     print(dikt)
     return json.dumps(dikt)
 
+def build_json_from_list(data):
+    id = 0
+    name = 1
+    price = 2
+    descr = 3
+    user_id = 4
+
+    dikt = {}
+
+    for adat in data:
+        temp = {}
+        temp['name'] = adat[name]
+        temp['price'] = adat[price]
+        temp['descr'] = adat[descr]
+        temp['user_id'] = adat[user_id]
+
+        dikt[str(adat[id])] = temp
+
+    return json.dumps(dikt)
+
+
 def get_one_product(id):
     session = Session()
     list = []
@@ -75,9 +87,38 @@ def get_product_on_page():
     list = []
     query = session.query(Product.Product).get()
 
+def get_prod_by_user(id):
+    session = Session()
+    query = session.execute(
+                    "SELECT * FROM product"
+                    " WHERE product.user_id=:param",
+                    {"param": id}
+            ).fetchall()
+
+    return query
+
+
+@app.route("/product")
+def list():
+    data = get_all_product()
+    return build_json(data)
+
+@app.route("/product/<id>")
+def list_one(id):
+    data = get_one_product(id)
+    return build_json(data)
+
+@app.route("/product/user/<id>")
+def get_product_by_user(id):
+    data = get_prod_by_user(id) #give back a list with tuples
+
+    return build_json_from_list(data)
+
+
 if __name__ == "__main__":
-    app.secret_key ='KEcskE'
-    app.run(
-        port=8080,
-        debug=True
-    )
+
+     app.secret_key ='KEcskE'
+     app.run(
+         port=50098,
+         debug=True
+     )
