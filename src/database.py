d4009ac16f591319c7172bce1ce69939dd7ad9cb
Balogh David <balogh.david1015@gmail.com>
2018-08-29 13:30:48 +0200

    Heroku deployment

--- src/database.py
+++ src/database.py
@@ -1,17 +1,28 @@
 import os
-
+import psycopg2
+import urllib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
-user_name = os.environ.get('PSQL_USER_NAME')
-password = os.environ.get('PSQL_PASSWORD')
-engine = create_engine("postgresql://{user_name}:{password}@localhost:5432/shitwishproducts".format(
-            user_name=user_name,
-            password=password
-            ))
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
-Base.metadata.create_all(engine)
+# user_name = os.environ.get('PSQL_USER_NAME')
+# password = os.environ.get('PSQL_PASSWORD')
+# engine = create_engine("postgresql://{user_name}:{password}@localhost:5432/shitwishproducts".format(
+#             user_name=user_name,
+#             password=password
+#             ))
+
+Base.metadata.create_all(connection)
 
