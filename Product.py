d8c753fb111626e33dd82b23b1fb27412b2fc596
Csonti <csontostamas1991@gmail.com>
2018-08-30 11:21:08 +0200

    zibi sirt

--- Product.py
+++ Product.py
@@ -9,13 +9,13 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
     is_incart = db.Column(db.Boolean, nullable=False)
     img = db.Column(db.String(255), nullable=False)
     is_active = db.Column(db.Boolean, nullable=False)
 
-    def __init__(self, name, price, user_id, img="https://imgur.com/a/C94poxI", description=None):
+    def __init__(self, name, price, user_id, img="https://imgur.com/a/C94poxI", description="Description is not available"):
         self.name = name
         self.price = price
         self.user_id = user_id
